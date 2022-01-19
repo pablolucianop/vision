4934b095412d3ba46a2f00da72dfc417c9970813
pablolucianop <pablolucianop@gmail.com>
2022-01-19 15:53:42 -0300

    with p5

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let capture;
+let aa =0
+let posterLevels =5
+function setup() {
+
+  createCanvas(320, 540);
+  button = createButton('click me');
+  button.mousePressed(changeBG);
+  button.position(0, 0);
+  
+  button = createButton('+');
+  button.position(130, 0);
+  button.mousePressed(plusPosterLevels);
+  
+  button = createButton('-');
+  button.position(250, 0);
+  button.mousePressed(minusPosterLevels);
+
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  capture.hide();
+  
+  function changeBG() {
+    if(aa===0)
+      {aa=1000;}else{aa=0;}
+    } 
+  function plusPosterLevels() {
+    posterLevels ++
+  }   
+  function minusPosterLevels() {
+    posterLevels --
+  }  
+}
+
+function draw() {
+  image(capture, 0, 0, 320, 240);
+  background(255);
+  push();
+  translate(width,0);
+  scale(-1, 1);
+
+    image(capture, aa,0, 320, 240);
+  
+  filter(GRAY);
+  filter(POSTERIZE, posterLevels)
+  image(capture, 0,240, 320, 240);
+  pop();
+ 
+
+
+
+
+
+}
