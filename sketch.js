f4aa8ded3da6d7c97b10c55e10c5cf1679b03ce3
pablolucianop <pablolucianop@gmail.com>
2022-01-19 18:06:52 -0300

    props

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 let capture;
 let aa =0
 let posterLevels =5
+let heigthCamera = 240
+let lengtCamera = 240* 1.333
+let mobile = false
+
 function setup() {
 
 
   
-  createCanvas(320, 540);
+  createCanvas(320, 700);
+  button = createButton('prop');
+  button.mousePressed(prop);
+  button.position(0, heigthCamera*2+ 60);
+
   button = createButton('guardar');
   button.mousePressed(guardar);
-  button.position(0, 600);
+  button.position(0, heigthCamera*2);
 
   button = createButton('click me');
   button.mousePressed(changeBG);
@@ -34,7 +42,12 @@ function setup() {
     posterLevels ++
   }  
   function guardar() {
-     saveFrames('out', 'png', 1, 1);
+     saveFrames('out', 'jpg', 1, 1);
+  } 
+
+  function prop() {
+    mobile = !mobile
+    console.log('mobile', mobile)
   } 
   function minusPosterLevels() {
     posterLevels --
@@ -42,20 +55,32 @@ function setup() {
 }
 
 function draw() {
-  image(capture, 0, 0, 320, 240);
-  background(255);
+
+let temp;
+
+if(mobile){
+
+lengtCamera = 240
+heigthCamera = lengtCamera* 1.333
+}else{
+heigthCamera = 240
+lengtCamera = heigthCamera* 1.333
+}
+
+
+  image(capture, 0, 0, lengtCamera, heigthCamera);
+  background(100);
   push();
   translate(width,0);
   scale(-1, 1);
 
-    image(capture, aa,0, 320, 240);
+    image(capture, aa,0, lengtCamera, heigthCamera);
   
   filter(GRAY);
   filter(POSTERIZE, posterLevels)
-  image(capture, 0,240, 320, 240);
+  image(capture, 0,heigthCamera, lengtCamera, heigthCamera);
   pop();
  
-  img = loadImage('assets/moonwalk.jpg'); // Cargar la imagen
 
 
 
