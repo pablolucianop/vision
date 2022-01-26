bd9afc258497b4a331c567ca63119fe35521d4b3
pablolucianop <pablolucianop@gmail.com>
2022-01-26 13:56:52 -0300

    restart

--- sketch.js
+++ sketch.js
@@ -4,77 +4,16 @@ let posterLevels =5
 let heigthCamera = 240
 let lengtCamera = 240* 1.333
 let mobile = false
-let mirror = -1
+
 
 let switchFlag = false;
 let switchBtn;
-
 var options = {
      video: {
-        
-         facingMode: {
-          exact: "user"
-        }
+         facingMode: "environment"
      }
    };
 
-
-
-function setup() {
-
-  capture = createCapture(options);
-  capture.size(320, 240);
-  capture.hide();
-
-
-  switchBtn = createButton('Switch Camera');
-  switchBtn.position(19, 19);
-  switchBtn.mousePressed(switchCamera);
-  
-  createCanvas(320, 700);
-  button = createButton('prop');
-  button.mousePressed(prop);
-  button.position(0, heigthCamera*2+ 60);
-
-
-  button = createButton('guardar');
-  button.mousePressed(guardar);
-  button.position(0, heigthCamera*2);
-
-  button = createButton('click me');
-  button.mousePressed(changeBG);
-  button.position(0, 0);
-  
-  button = createButton('+');
-  button.position(130, 0);
-  button.mousePressed(plusPosterLevels);
-  
-  button = createButton('-');
-  button.position(250, 0);
-  button.mousePressed(minusPosterLevels);
-  var constraints = {
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment"
-      }
-    }    
-    // video: {
-    //   facingMode: "user"
-    // } 
-  };
-
-
-
-
-  
-  function changeBG() {
-    if(aa===0)
-      {aa=1000;}else{aa=0;}
-    } 
-  function plusPosterLevels() {
-    posterLevels ++
-  }  
 
 function switchCamera()
 {
@@ -85,9 +24,7 @@ function switchCamera()
    options = {
      video: {
         
-         facingMode: {
-          exact: "environment"
-        }
+         facingMode: "environment"
      }
    };
 
@@ -109,9 +46,50 @@ function switchCamera()
   
 }
 
+function setup() {
+
+
+  
+  createCanvas(320, 700);
+  button = createButton('prop');
+  button.mousePressed(prop);
+  button.position(0, heigthCamera*2+ 60);
+
+  button = createButton('guardar');
+  button.mousePressed(guardar);
+  button.position(0, heigthCamera*2);
+
+  button = createButton('click me');
+  button.mousePressed(changeBG);
+  button.position(0, 0);
+  
+  switchBtn = createButton('Switch Camera');
+  switchBtn.position(19, 19);
+  switchBtn.mousePressed(switchCamera);
+
+  button = createButton('+');
+  button.position(130, 0);
+  button.mousePressed(plusPosterLevels);
+  
+  button = createButton('-');
+  button.position(250, 0);
+  button.mousePressed(minusPosterLevels);
+
+  capture = createCapture(options);
+  capture.size(320, 240);
+  capture.hide();
+  
+  function changeBG() {
+    if(aa===0)
+      {aa=1000;}else{aa=0;}
+    } 
+  function plusPosterLevels() {
+    posterLevels ++
+  }  
   function guardar() {
      saveFrames('out', 'jpg', 1, 1);
   } 
+
   function prop() {
     mobile = !mobile
     console.log('mobile', mobile)
@@ -123,39 +101,33 @@ function switchCamera()
 
 function draw() {
 
-  let temp;
+let temp;
 
-  if(mobile){
+if(mobile){
 
-  lengtCamera = 240
-  heigthCamera = lengtCamera* 1.333
-  }else{
-  heigthCamera = 240
-  lengtCamera = heigthCamera* 1.333
-  }
+lengtCamera = 240
+heigthCamera = lengtCamera* 1.333
+}else{
+heigthCamera = 240
+lengtCamera = heigthCamera* 1.333
+}
 
-  image(capture, 0, 0, lengtCamera, heigthCamera);
+let aaa=  image(capture, 0, 0, lengtCamera, heigthCamera);
+ 
   background(100);
   push();
   translate(width,0);
-  scale(1, 1);
+  scale(-1, 1);
 
-  image(capture, aa,0, lengtCamera, heigthCamera);
+    image(capture, aa,0, lengtCamera, heigthCamera);
   
   filter(GRAY);
   filter(POSTERIZE, posterLevels)
   image(capture, 0,heigthCamera, lengtCamera, heigthCamera);
   pop();
-
  
 
 
 
 
 }
-
-
-
-
-
-
